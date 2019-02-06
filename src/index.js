038023b416c5c71b45725706e9382850b7dd4874
Joseph Cobhams <joseph.cobhams@andela.com>
2019-02-06 21:35:09 +0100

    modify build setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React, { Fragment} from 'react';
+import React, { Fragment } from 'react';
 import ReactDom from 'react-dom';
 
 class Welcome extends React.Component {
   render () {
     return (
       <Fragment>
-      <h1>Hurray! your react app has started</h1>
-      <p>Begin creating great thing</p>
-    </Fragment>
+        <h1>Hurray! your react app has started</h1>
+        <p>Begin creating great thing</p>
+      </Fragment>
     );
   }
 }
